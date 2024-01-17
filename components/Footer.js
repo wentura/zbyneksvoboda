c5ff42cee8815b602ca18956222f0ad59bdcd698
Zbynek Svoboda <wentura17@seznam.cz>
2024-01-17 23:25:12 +0100

    footer 2024

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
       <div className="flex flex-col-reverse justify-between pt-5 pb-10 lg:flex-row">
-        <p className="text-sm text-gray-600">© 2023 Zbyněk Svoboda</p>
+        <p className="text-sm text-gray-600">© 2024 Zbyněk Svoboda</p>
         {/* <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
           <li>
             <Link
