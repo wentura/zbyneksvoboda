3b22f39710a712b0711385c572a89261ce743db7
Zbynek Svoboda <wentura17@seznam.cz>
2024-02-21 14:37:58 +0100

    font jen bezpatkovy a zmizel jsem tlacitko

--- components/Hero.js
+++ components/Hero.js
@@ -18,28 +18,25 @@ export default function Hero() {
   return (
     <div className="w-full sm:bg-slate-100 shadow-xl">
       <div className="relative flex flex-wrap max-w-full justify-between md:justify-start lg:max-w-screen-2xl px-4 md:py-0 mx-auto max-h-96 md:max-h-[700px] overflow-hidden">
-        <div className="my-12 lg:max-w-xl xl:max-w-2xl mx-0 md:mx-4 lg:mx-0">
+        <div className="w-full my-12 lg:max-w-xl xl:max-w-2xl mx-0 md:mx-4 lg:mx-0">
           <div className="mx-auto">
             <div className="relative mb-4 lg:mt-16 p-8 bg-slate-100/70 backdrop-blur-sm rounded-2xl z-20">
-              <div>
-                <p className="inline-block mb-0 text-base font-bold md:font-normal text-gray-900">
-                  jsem tvůrce webů na volné noze
-                </p>
-              </div>
+              <p className="inline-block mb-0 text-lg font-bold md:font-normal text-gray-900">
+                jsem
+              </p>
               <h2 className="max-w-lg mb-2 text-2xl font-black text-gray-900 sm:text-4xl sm:leading-none">
                 Zbyněk Svoboda
               </h2>
-              <h1 className="max-w-xl py-4 md:pb-20 text-5xl font-extrabold text-gray-900 sm:text-7xl sm:leading-none uppercase">
-                tvořím weby
-              </h1>
-              <p className="text-base text-gray-700 max-w-xl">
-                svými weby žiju a za svou prací si stojím.
-              </p>
-              <Link href="#about_me">
-                <p className="bg-green-300 text-lg w-fit my-4 px-4 py-2 rounded-xl shadow-md mx-auto md:mx-0 text-gray-900 font-extrabold zs-shadow">
+              <div className="max-w-xl pt-12 md:pb-20 text-5xl font-extrabold text-gray-900 sm:text-7xl sm:leading-none uppercase">
+                tvůrce webů a <br className="block md:hidden" />
+                webových aplikací
+              </div>
+
+              {/* <Link href="#about_me">
+                <p className="bg-green-300 text-lg w-fit mb-8 px-4 py-2 rounded-xl shadow-md mx-auto md:mx-0 text-gray-900 font-extrabold zs-shadow">
                   Jsem Vám k dispozici
                 </p>
-              </Link>
+              </Link> */}
               <p className="text-sm">
                 {/* Začněme dnes, ale pracovat na Vašem projektu mohu až {date} */}
               </p>
